43b30b959dd9e1ad15cf0af278e36db2a8b9fb88
blakolukas <lucasblacovicz@gmail.com>
2024-08-14 20:28:57 -0300

    att

--- algoritmos.py
+++ algoritmos.py
@@ -1,5 +1,18 @@
+#1
+def fat(n):
+    if n==1:
+        return 1
+    return n*fat(n-1)
 
+print(fat(4))
 
+#2
+def som(n):
+    if n==0:
+        return 0
+    return n+som(n-1)
+
+print(som(4))
 
 #3
 def fib(n):
@@ -30,6 +43,27 @@ def isPal(s):
     return False
 
 print(isPal("ovo"))
+
+#6
+def convBase2(n):
+    if n==0:
+        return ""  
+    if n%2==0:
+        return convBase2(int(n/2))+"0"
+    return convBase2(int(n/2))+"1"
+
+print(convBase2(3))
+#9
+def findSubStr(str, match):
+    if len(str)<len(match):
+        return False
+    if str[0]==match[0]:
+        return True
+    if str[0]==match[0]:
+        return findSubStr(str[1:len(str)],match[1:len(match)])
+    return findSubStr(str[1:len(str)],match)
+    
+print(findSubStr("eu amo", "choro"))
 
 #10
 def nroDigit(n):
